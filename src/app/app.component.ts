a8ffdd969e94acd445bed654d4c77390f5454e83
Aaron Tuil <aaront@ravtech.co.il>
2017-11-21 19:07:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  sign = "X";
   title = 'app';
+  items = [1,2,3];
+  boxes = [];
+
+  clicked() {
+    if (this.sign == "O") {
+      this.sign = "X";
+    } else {
+      this.sign = "O"
+    }
+
+    this.checkWin();
+  }
+
+  checkWin() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RectangleComponent } from './rectangle/rectangle.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RectangleComponent
   ],
   imports: [
     BrowserModule
